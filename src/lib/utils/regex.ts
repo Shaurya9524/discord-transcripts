9f9fd057914892e1087368f2ad23f80edfa92444
ShadowOP6222 <blues9524@gmail.com>
2023-08-02 13:12:26 +0530

    regular expressions update for allowing emoji characters in name

--- src/lib/utils/regex.ts
+++ src/lib/utils/regex.ts
@@ -2,9 +2,9 @@ export const regex = {
   url: /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b/,
   imageUrl: /https?:\/\/(?:www\.)?[\w-]+(?:\.[\w-]+)+[\w.,@?^=%&:/~+#-]*\.(?:png|jpg|jpeg|webp)/,
   hexColor: /^#?([a-f0-9]{8}|[a-f0-9]{6}|[a-f0-9]{4}|[a-f0-9]{3})$/i,
-  roleObject: /RoleObject\(([^),]+)(?:,\s*([^)]+))?\)/g,
-  channelObject: /ChannelObject\(([a-zA-Z0-9_-]+)\)$/g,
-  userObject: /UserObject\(([^()]+)\)$/g
+  roleObject: /RoleObject\(([^),]+|[\p{Emoji}])(?:,\s*([^)]+))?\)/gu,
+  channelObject: /ChannelObject\(([^()]+|[\p{Emoji}])\)$/gu,
+  userObject: /UserObject\(([^()]+|[\p{Emoji}])\)$/gu
 }
 
 export function isURL(str: string) {
